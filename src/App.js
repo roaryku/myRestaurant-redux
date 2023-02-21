289c0cdc93e8ca2eefcf91b19980ef90c4c43195
Rodika <raryku@yahoo.com>
2023-02-21 11:37:41 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount, decrementByAmount,  multementByAmount} from './redux/counter';
 import './App.css';
 
 function App() {
+
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Score: {count}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      <button onClick={() => dispatch(decrementByAmount(5))}>-5</button>
+      <button onClick={() => dispatch(multementByAmount(5))}>*5</button>
     </div>
   );
 }
